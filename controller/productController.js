79ed16979d7b8f685cb99110a05c02d0d32def6b
valensartorelli <valen.sartorelli@gmail.com>
2021-04-25 22:16:57 -0300

    Valen - Agrego forms de edicion de producto

--- controller/productController.js
+++ controller/productController.js
@@ -5,6 +5,18 @@ let productController = {
     leerTodos: function() {
         console.log('leo productos desde data');
         return productos
+    },
+    index: (req, res) => {
+        // leo todo el array de products en el controlador productController
+        const listado = productController.leerTodos();
+        // envio el array product a la vista para que la recorra EJS
+        res.render('product', { listado });
+    },
+    create: (req, res) => {
+        return res.render('alta')
+    },
+    edit: (req, res) => {
+        return res.render('detailEdit')
     }
 }
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controladorProduct = require('../controller/productController');
 
-router.get('/', (req, res) => {
-    // leo todo el array de products en el controlador productController
-    const listado = controladorProduct.leerTodos();
-    // envio el array product a la vista para que la recorra EJS
-    console.log('volvi del controlador')
+router.get('/', controladorProduct.index);
 
-    res.render('product', { listado });
-})
+router.get('/create', controladorProduct.create);
+
+router.get('/edit', controladorProduct.edit);
 
 module.exports = router;
